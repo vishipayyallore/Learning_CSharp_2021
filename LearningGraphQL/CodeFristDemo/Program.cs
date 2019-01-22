b1a17631930c0d774b63ecbcc4c9c5c17a6ad9ff
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-01-22 09:56:08 +0530

    Added new project

---
+++ LearningGraphQL/CodeFristDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeFristDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
