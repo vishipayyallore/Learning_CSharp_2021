3f7eaa9d191e72c73876068759b1b782e4fb5200
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-08 10:50:48 +0530

    small modifications

--- LearningCSharp7/Source/Learn.TuplesDemo/Student.cs
+++ LearningCSharp7/Source/Learn.TuplesDemo/Student.cs
@@ -4,13 +4,20 @@ namespace Learn.TuplesDemo
 {
     public class Student
     {
+
+        public Student()
+        {
+        }
+
         public int Id { get; set; }
 
         public string First { get; set; }
 
         public string Last { get; set; }
 
-        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age { get; }
 
         public float Salary { get; set; }
     }
