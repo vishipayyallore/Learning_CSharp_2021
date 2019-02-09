dbcf0621ec85d9c7d0d6611c6857f95b8e858876
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-09 22:23:36 +0530

    Simple Unit Test

---
+++ LearningCSharp7/Test/Learn.TuplesDemoTests/TuplesTypeDemoTests.cs
@@ -0,0 +1,19 @@
+using Learn.CSharp7.Common;
+using Learn.TuplesDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learn.TuplesDemoTests
+{
+    [TestClass]
+    public class TuplesTypeDemoTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IDemoDataType tupleDataTypeDemo = new DemoTuples();
+
+            tupleDataTypeDemo.ShowDemo(System.ConsoleColor.Green);
+        }
+
+    }
+}
