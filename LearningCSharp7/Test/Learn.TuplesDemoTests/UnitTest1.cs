a7ff02c12aac34eb27b99f5726dabb3ac2ec8893
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-09 22:20:08 +0530

    Added Unit Test project

---
+++ LearningCSharp7/Test/Learn.TuplesDemoTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Learn.TuplesDemoTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
