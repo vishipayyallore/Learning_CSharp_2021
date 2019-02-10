2cafa7b7a669658f104367592517bfd393081580
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-10 22:36:28 +0530

    Small Enhancements

--- LearningCSharp7/Source/Learn.TuplesDemo/TuplesTypeDemo.cs
+++ LearningCSharp7/Source/Learn.TuplesDemo/TuplesTypeDemo.cs
@@ -28,13 +28,21 @@ namespace Learn.TuplesDemo
             (string First, string Last) fullName3 = GetFullName3();
             WriteLine($"Full Name: {fullName3.First} {fullName3.Last}");
 
+            // Deconstruction
+            var (First, Last) = ("Shiva", "Sai");
+            WriteLine($"Full Name: {First} {Last}");
+
             // Tuple Type
             (string First, string Last) fullNameN = ("Manish", "Aggarwal");
             WriteLine($"Full Name: {fullNameN.First} {fullNameN.Last}");
 
-
         }
 
+        /// <summary>
+        /// Tuple Type (string FName, string LName).  
+        /// Tuple Literal (FName: "Manish", LName: "Aggarwal");
+        /// </summary>
+        /// <returns></returns>
         private static (string FName, string LName) GetFullName3()
         {
             return (FName: "Manish", LName: "Aggarwal");
