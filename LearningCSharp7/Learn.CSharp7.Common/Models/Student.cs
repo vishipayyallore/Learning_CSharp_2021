f83ba55441a4c0f76a4ac4eebe6695eb642d73c8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-10 22:21:47 +0530

    Creating the model classes

---
+++ LearningCSharp7/Learn.CSharp7.Common/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learn.CSharp7.Common.Models
+{
+
+    public class Student
+    {
+
+        public Student()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string First { get; set; }
+
+        public string Last { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public int Age { get; }
+
+        public float Salary { get; set; }
+
+        public List<Subject> Subjects { get; set; }
+    }
+
+}
---
+++ LearningCSharp7/Learn.CSharp7.Common/Models/Subject.cs
@@ -0,0 +1,11 @@
+﻿namespace Learn.CSharp7.Common.Models
+{
+
+    public struct Subject
+    {
+        public string Name { get; set; }
+
+        public decimal Marks { get; set; }
+    }
+
+}
