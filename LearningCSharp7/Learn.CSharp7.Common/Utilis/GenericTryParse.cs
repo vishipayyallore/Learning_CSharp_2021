a660e357f8e9b27a890872d97c498af77ca1875e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-11 10:29:50 +0530

    Created Generic Try Parse Method.

---
+++ LearningCSharp7/Learn.CSharp7.Common/Utilis/GenericTryParse.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace Learn.CSharp7.Common.Utilis
+{
+
+    public class GenericTryParse
+    {
+        public void TryParse<T>(string value, ref T destinationType)
+        {
+            var dataType = destinationType.GetType();
+            var tryParseMethod = dataType.GetMember("TryParse");
+
+            object[] parameters = new object[] { value, null };
+
+            var output = ((MethodInfo)tryParseMethod[0]).Invoke(dataType, parameters);
+            destinationType = ((bool)output ? (T)parameters[1] : destinationType);
+        }
+    }
+
+}
