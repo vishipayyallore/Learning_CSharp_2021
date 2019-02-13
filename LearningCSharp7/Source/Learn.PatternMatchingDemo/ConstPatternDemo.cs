2f35f6d27ea7963614a78ea1e504e7fb6ae1e832
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-13 23:29:39 +0530

    New Project added

---
+++ LearningCSharp7/Source/Learn.PatternMatchingDemo/ConstPatternDemo.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Learn.PatternMatchingDemo
+{
+    public class ConstPatternDemo
+    {
+    }
+}
---
+++ LearningCSharp7/Source/Learn.PatternMatchingDemo/VarPatternDemo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Learn.PatternMatchingDemo
+{
+    class VarPatternDemo
+    {
+    }
+}
