c283845b846a67fdfa67eb957e9ea5b3bec2de6e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-18 22:30:44 +0530

    Discard Demo

--- LearningCSharp7/Source/Learn.MiscDemo/DiscardsDemo.cs
+++ LearningCSharp7/Source/Learn.MiscDemo/DiscardsDemo.cs
@@ -1,10 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Learn.CSharp7.Common;
+using System;
+using static System.Console;
 
 namespace Learn.MiscDemo
 {
-    class DiscardsDemo
+    /// <summary>
+    /// Discard => write-only variable whose name is _ (underscore character).
+    /// </summary>
+    public class DiscardsDemo : IDataTypeDemo
     {
+        public void ShowDemo(ConsoleColor foreGroundColor)
+        {
+            ForegroundColor = foreGroundColor;
+
+            var (_, Name, _, Salary) = (1, "Shiva Sai", 25, 123.45);
+            WriteLine($"Name: {Name}, Salary: {Salary}");
+        }
+
     }
+
 }
