04d35011d0f7ca0109c969eb65223652b7b7420d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-20 23:12:50 +0530

    Added Id property

--- LearningCSharp7/Source/Learn.MongoDbDemo/StoreNRetrievePerson.cs
+++ LearningCSharp7/Source/Learn.MongoDbDemo/StoreNRetrievePerson.cs
@@ -1,5 +1,4 @@
 ﻿using MongoDB.Driver;
-using System;
 
 namespace Learn.MongoDbDemo
 {
@@ -13,15 +12,18 @@ namespace Learn.MongoDbDemo
             var dataBase = mongoDbClient.GetDatabase("test");
             var persons = dataBase.GetCollection<Person>("persons");
 
-            var person1 = new Person { Age = 24, FirstName = "Alex", LastName = "Pandian" };
+            var person1 = new Person { Id = 125, Age = 24, FirstName = "Alex", LastName = "Pandian" };
 
             persons.InsertOne(person1);
+
         }
 
     }
 
     public class Person
     {
+        public int Id { get; set; }
+
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
