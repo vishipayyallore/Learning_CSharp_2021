54ab48407182b90304ce727fa44736a27ca530a9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-26 23:15:16 +0530

    Demo on Pattern Matching

---
+++ LearningCSharp7/Source/Learn.MiscDemo/PatternMatchingDemo.cs
@@ -0,0 +1,16 @@
+﻿using Learn.CSharp7.Common;
+using static System.Console;
+
+namespace Learn.MiscDemo
+{
+    public class PatternMatchingDemo : IDataTypeDemo
+    {
+        public string Title => GetType().Name;
+
+        public void ShowDemo()
+        {
+            // is expression pattern matching
+            WriteLine("");
+        }
+    }
+}
