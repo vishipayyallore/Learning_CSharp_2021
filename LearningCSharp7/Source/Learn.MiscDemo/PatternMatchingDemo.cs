9e8493779bcd107becfb7ac0e4606a9422b3d092
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-02-27 23:14:03 +0530

    Enhanced program

--- LearningCSharp7/Source/Learn.MiscDemo/PatternMatchingDemo.cs
+++ LearningCSharp7/Source/Learn.MiscDemo/PatternMatchingDemo.cs
@@ -11,7 +11,6 @@ namespace Learn.MiscDemo
 
         public void ShowDemo()
         {
-
             var employee = new Employee { Id = "E101", Name = "Anil", TaskCodes = new List<int> { 101, 102, 103, 104 } };
             DisplayInformation(employee);
 
@@ -25,6 +24,25 @@ namespace Learn.MiscDemo
             if (person is Employee employee)
             {
                 WriteLine($"Id= {employee.Id}, Name= {employee.Name}, Task Codes= {string.Join<int>(", ", employee.TaskCodes)} ");
+            }
+            if (person is Manager manager)
+            {
+                WriteLine($"Id= {manager.Id}, Name= {manager.Name}, Repotees= {string.Join<string>(", ", manager.Repotees)} ");
+            }
+        }
+
+        private void DisplayInformationUsingSwitch(object person)
+        {
+            switch (person)
+            {
+                case Employee employee:
+                    WriteLine($"Id= {employee.Id}, Name= {employee.Name}, Task Codes= {string.Join<int>(", ", employee.TaskCodes)} ");
+                    break;
+                case Manager manager:
+                    WriteLine($"Id= {manager.Id}, Name= {manager.Name}, Repotees= {string.Join<string>(", ", manager.Repotees)} ");
+                    break;
+                default:
+                    break;
             }
         }
 
