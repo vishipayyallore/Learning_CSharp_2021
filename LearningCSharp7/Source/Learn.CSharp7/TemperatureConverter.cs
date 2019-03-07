a9bee10134637674312ef7f5271017f2bf783a0d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-03-08 00:02:14 +0530

    Added two basic method.

--- LearningCSharp7/Source/Learn.CSharp7/TemperatureConverter.cs
+++ LearningCSharp7/Source/Learn.CSharp7/TemperatureConverter.cs
@@ -3,6 +3,16 @@
     public class TemperatureConverter
     {
 
+        public double Fahrenheit(double celsius)
+        {
+            return (celsius * 9.0 / 5.0) + 32.0;
+        }
+
+        public double Celsius(double fahrenheit)
+        {
+            return (fahrenheit - 32.0) * 5.0 / 9.0;
+        }
+
     }
 
 }
