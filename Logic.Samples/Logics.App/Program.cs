645d0fb431435fdc8f0afeaba81fa6d147d1eed9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2019-05-09 08:24:56 +0530

    Simple Console Application Added

---
+++ Logic.Samples/Logics.App/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Logics.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
