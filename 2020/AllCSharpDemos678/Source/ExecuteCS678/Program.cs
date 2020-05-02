a6136cdd418995af7f877b0747000f2c62c459c1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-02 22:49:11 +0530

    ..

---
+++ 2020/AllCSharpDemos678/Source/ExecuteCS678/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExecuteCS678
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
