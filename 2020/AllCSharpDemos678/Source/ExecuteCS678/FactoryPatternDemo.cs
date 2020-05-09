9096e09d85cea564852f24f2d8bb98e244d4a742
Viswanatha Swamy <vishipayyallore@yahoo.com>
2020-05-09 23:23:51 +0530

    Update FactoryPatternDemo.cs

--- 2020/AllCSharpDemos678/Source/ExecuteCS678/FactoryPatternDemo.cs
+++ 2020/AllCSharpDemos678/Source/ExecuteCS678/FactoryPatternDemo.cs
@@ -1,19 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace ExecuteCS678
+﻿namespace ExecuteCS678
 {
     class FactoryPatternDemo
     {
     }
 
+    public interface IShape
+    {
+        ShapeResponse GetDetails();
+    }
+
+    public class Rectangle : IShape
+    {
+
+        public ShapeResponse GetDetails()
+        {
+            var shapeResponse = new ShapeResponse()
+            {
+                Name = nameof(Square),
+                Description = "No details."
+            };
+
+            return shapeResponse;
+        }
+    }
+
+    public class Square : IShape
+    {
+        public ShapeResponse GetDetails()
+        {
+            var shapeResponse = new ShapeResponse()
+            {
+                Name = nameof(Square),
+                Description = "No details."
+            };
+
+            return shapeResponse;
+        }
+    }
+
+    public class NullShape : IShape
+    {
+        public ShapeResponse GetDetails()
+        {
+            var shapeResponse = new ShapeResponse()
+            {
+                Name = nameof(NullShape),
+                Description = "No details."
+            };
+
+            return shapeResponse;
+        }
+    }
 
     public class ShapeResponse
     {
         public string Name { get; set; }
 
-
+        public string  Description { get; set; }
     }
 
 }
