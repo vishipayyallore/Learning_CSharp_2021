ae0433265e9841f95d47ebec690b86206204c351
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-13 00:31:21 +0530

    ..

---
+++ AllCSharpDemos/Try.DSA/CalendarOperations.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Try.DSA
+{
+    public class CalendarOperations : ICalendarOperations
+    {
+
+        public string[] GetMonthNames()
+        {
+            string[] months = new string[12];
+
+            for (int month = 1; month <= 12; month++)
+            {
+                months[month - 1] = new DateTime(DateTime.Now.Year, month, 1)
+                                        .ToString("MMMM", CultureInfo.CreateSpecificCulture("en-in"));
+            }
+
+            return months;
+        }
+
+        public List<DateTime> GetDatesInAMonth(int month, int year)
+        {
+            return Enumerable.Range(1, DateTime.DaysInMonth(year, month))
+                .Select(x => new DateTime(year, month, x))
+                .ToList();
+        }
+
+    }
+
+}
---
+++ AllCSharpDemos/Try.DSA/ICalendarOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Try.DSA
+{
+
+    public interface ICalendarOperations
+    {
+        string[] GetMonthNames();
+
+        List<DateTime> GetDatesInAMonth(int month, int year);
+    }
+
+}
