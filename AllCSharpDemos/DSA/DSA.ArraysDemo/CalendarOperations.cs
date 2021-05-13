c7068051811af8cea34f663a9b3a6fb6ce36fdc9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-05-13 22:29:45 +0530

    Today's Program

--- AllCSharpDemos/DSA/DSA.ArraysDemo/CalendarOperations.cs
+++ AllCSharpDemos/DSA/DSA.ArraysDemo/CalendarOperations.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
-namespace Try.DSA
+namespace DSA.ArraysDemo
 {
     public class CalendarOperations : ICalendarOperations
     {
---
+++ AllCSharpDemos/DSA/Try.DSA/Program.cs
@@ -0,0 +1,46 @@
+﻿using DSA.ArraysDemo;
+using System;
+
+using static System.Console;
+
+namespace Try.DSA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (DateTime.TryParse("02/02/2016", out var parsedDateTime))
+            {
+                WriteLine($"Parsed: {parsedDateTime}");
+            }
+            WriteLine($"Parsed value outside scope: {parsedDateTime}");
+
+            if (DateTime.TryParse("InvalidDate", out parsedDateTime))
+            {
+                WriteLine($"Parsed: {parsedDateTime}");
+            }
+            WriteLine($"Parsed value outside scope: {parsedDateTime}");
+
+            _ = int.TryParse("abc", out var i);
+            WriteLine($"i = {i}"); // default value
+
+            CalendarOperations calendarOperations = new();
+
+            var dates = calendarOperations.GetDatesInAMonth(2, 2020);
+            dates = calendarOperations.GetDatesInAMonth(2, 2024);
+            dates = calendarOperations.GetDatesInAMonth(2, 2028);
+
+            int index = 1;
+            Array.ForEach(calendarOperations.GetMonthNames(), (month) =>
+            {
+                WriteLine($"{index++}. {month}");
+            });
+
+            WriteLine("\n\nPress any key ...");
+            Read();
+        }
+
+    }
+
+}
