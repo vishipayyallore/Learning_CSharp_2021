ffc7e606f9bce99c026073b6babef059f2e7a02d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-15 13:56:21 +0530

    Few Changes

--- AllCSharpDemos/CSharpV7Demos/V7Demos/ShowOutVariablesDemo.cs
+++ AllCSharpDemos/CSharpV7Demos/V7Demos/ShowOutVariablesDemo.cs
@@ -2,7 +2,7 @@
 
 namespace V7Demos
 {
-    public class Class1
+    public class ShowOutVariablesDemo
     {
     }
 }
--- AllCSharpDemos/DSA/Try.DSA/Program.cs
+++ AllCSharpDemos/DSA/Try.DSA/Program.cs
@@ -65,22 +65,33 @@ namespace Try.DSA
             _ = int.TryParse("abc", out var i);
             WriteLine($"i = {i}"); // default value
 
-            //CalendarOperations calendarOperations = new();
-
-            //var dates = calendarOperations.GetDatesInAMonth(2, 2020);
-            //dates = calendarOperations.GetDatesInAMonth(2, 2024);
-            //dates = calendarOperations.GetDatesInAMonth(2, 2028);
-
-            //int index = 1;
-            //Array.ForEach(calendarOperations.GetMonthNames(), (month) =>
-            //{
-            //    WriteLine($"{index++}. {month}");
-            //});
-
             WriteLine("\n\nPress any key ...");
             Read();
         }
 
+        //private static T ParseData<T>(string data)
+        //{
+        //    if (DateTime.TryParse(data, out T parsedData))
+        //    {
+        //        WriteLine($"Parsed: {parsedData}");
+        //    }
+
+        //    return parsedData;
+        //}
+
     }
 
 }
+
+
+//CalendarOperations calendarOperations = new();
+
+//var dates = calendarOperations.GetDatesInAMonth(2, 2020);
+//dates = calendarOperations.GetDatesInAMonth(2, 2024);
+//dates = calendarOperations.GetDatesInAMonth(2, 2028);
+
+//int index = 1;
+//Array.ForEach(calendarOperations.GetMonthNames(), (month) =>
+//{
+//    WriteLine($"{index++}. {month}");
+//});
