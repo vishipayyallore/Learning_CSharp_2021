08dc7fdc951ad7fe5964cb5c3f8fc67c79622ed1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2021-07-20 20:55:12 +0530

    Today's work

--- AllCSharpDemos/MiniProjects/Try.WeatherUtility/Program.cs
+++ AllCSharpDemos/MiniProjects/Try.WeatherUtility/Program.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using WeatherUtility;
+
+using static System.Console;
 
 namespace Try.WeatherUtility
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IWeatherUtilities weatherUtilities = new WeatherUtilities();
+
+            var fahrenheit = 65;
+            var celsius = weatherUtilities.FahrenheitToCelsius(fahrenheit);
+            WriteLine($"{fahrenheit}°F equals {celsius}°C");
+
+            WriteLine("\n\nPress any key ...");
+            ReadKey();
         }
+
     }
+
 }
